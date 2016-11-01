73362ad432aab05e58f31b2694f3e33f3de5f76f
crash_man <crash_man@163.com>
2016-11-01 08:43:53 +0800

    add some new method of Grid and Snake class

--- Grid.java
+++ Grid.java
@@ -2,7 +2,22 @@
 *@author crash_man
 *@Time 2016-11-01
 */
+package com.crashman.snake;
+import java.util.Arrays;
+
 public class Grid
 {
-	private Snake snake;
+	public final boolean status[][];
+	private final int width;
+    private final int height;
+	private Node food;
+    private Snake snake;
+ 	// 初始方向默认设置为向左
+    private Direction snakeDirection = Direction.LEFT;
+
+    public Grid(int width, int height) {
+
+        this.width = width;
+        this.height = height;
+    }
 }
--- Snake.java
+++ Snake.java
@@ -2,7 +2,40 @@
 *@author crash_man
 *@Time 2016-11-01
 */
+package com.crashman.snake;
+import java.util.LinkedList;
+
 public class Snake
 {
+	private LinkedList<Node> body = new LinkedList<>();
+	public Node eat(Node food) {
 
+        if (!isNeighbor(body.getFirst(), food)) {
+        	// 相邻情况下的处理
+        }
+        // 如果food与头部相邻，则将food这个Node加入到body中，返回food
+        // 否则不做任何操作，返回null		
+    }
+    
+    private boolean isNeighbor(Node a, Node b) {
+        return Math.abs(a.getX() - b.getX()) + Math.abs(a.getY() - b.getY()) == 1;
+    }	
+
+    public Node move(Direction direction) {
+        // 根据方向更新贪吃蛇的body
+        // 返回移动之前的尾部Node
+    }
+
+    public Node getHead() {
+        return body.getFirst();
+    }
+
+    public Node addTail(Node area) {
+        this.body.addLast(area);
+        return area;
+    }
+
+    public LinkedList<Node> getBody() {
+        return body;
+    }
 }
